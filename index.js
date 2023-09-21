8a3e39d605a8224d9e38593e8d67986f1955aea5
mskDev0092 <102049709+mskDev0092@users.noreply.github.com>
2023-09-21 23:56:02 +0500

    1 file modified, 2 added, 2 deleted

--- index.js
+++ index.js
@@ -6,51 +6,43 @@ var app = express();
 app.use(express.json());
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-// your first API endpoint... 
 
-const notValid = (date) => date.toUTCString() === "Invalid Date"
-
-app.get('/api/:date', function(req, res) {
-
-  
+// My first API endpoint...
+const notValid = (date) => date.toUTCString() === 'Invalid Date';
+app.get('/api/:date', function (req, res) {
   // user date input
   let date = new Date(req.params.date);
-
-  if(notValid(date)){
-date = new Date(+req.params.date)
+  if (notValid(date)) {
+    date = new Date(+req.params.date);
   }
-  if(notValid(date)){
-res.json({error: "Invalid Date"})
-return;  
-}
-res.status(200).json({ unix: date.getTime(), utc: date.toUTCString() });
-    
+  if (notValid(date)) {
+    res.json({ error: 'Invalid Date' });
+    return;
+  }
+  res.status(200).json({ unix: date.getTime(), utc: date.toUTCString() });
 });
 
-app.get("/api" , (req,res) => {
+app.get('/api', (req, res) => {
   res.json({
     unix: new Date().getTime(),
-    utc: new Date().toUTCString()
-  })
-})
+    utc: new Date().toUTCString(),
+  });
+});
 
-
-
-
-const port = 3003;
+const port = 3000;
 
 // listen for requests :)
-var listener = app.listen(port, function() {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
